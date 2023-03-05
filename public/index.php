5d3bfd9eb799b752d49ef6e0294a2555a7c926a3
Felinto <emersong20.email@gmail.com>
2023-03-05 18:39:28 +0000

    generate uuid for temporary files

--- public/index.php
+++ public/index.php
@@ -2,14 +2,16 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Ramsey\Uuid\Uuid;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+	$uuid = Uuid::uuid4()->toString();
+	$response->getBody()->write("Hello world! UUID: " . $uuid);
+	return $response;
 });
 
 $app->run();
